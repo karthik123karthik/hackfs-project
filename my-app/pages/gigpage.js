37399b75d66e01e45d736ac891affd27a631b6fc
karthik g.k <kar221694@gmail.com>
2022-07-23 13:50:09 -0400

    new gig card added

--- my-app/pages/gigpage.js
+++ my-app/pages/gigpage.js
@@ -1,20 +1,30 @@
-
+import Link from "next/link"
 
 export default function Gig(){
     return(
-       <div className="gig-Card border w-[25vw] h-[60vh]">
-         <div className="gig-intro border bg-[url(https://source.unsplash.com/random)] bg-cover h-[50%]">
-           
-         </div>
-         <div className="gigcreator-intro border h-[30%]">
-            <section className="profile-section border h-[20%]"></section>
-            <section className="service-section border h-[60%]"></section>
-            <section className="rating-section border h-[20%]"></section>
-         </div>
-         <div className="gig-pricing flex justify-between border h-[20%]">
-             <div className="heart border w-[20%]"></div>
-             <div className="price border w-[30%]"></div>
-         </div>
+      <Link href="/gigs/smartcontract">
+       <div className="gig-Card m-2 flex flex-col  w-[35vw]  h-[75vh] bg-indigo-100">
+            <div className="profile-part h-[80%] ">
+               <div className="bg-red-800 h-[50%]"></div>
+                <div className="flex flex-row  p-3">
+                  <img src="./favicon.ico" className="w-[40px] mr-3"></img>
+                   <div className="flex flex-col">
+                   <strong>karthik G K</strong>
+                   <strong className="text-[#D6BC01]">Top Seller</strong>
+                   </div>
+                </div>
+                <div className="text-xl p-2 hover:text-lime-600">
+                  I will do best Smart contract design according to your requirements
+                </div>
+            </div>
+            <div className="rating-part h-[20%]  flex flex-row items-center justify-between  p-3">
+                <img src="./heart.png" className="w-[40px]"></img>
+                <div className="text-lime-900 text-sm">
+                   <strong> Starting at Rupees</strong>
+                   <p>20 USD</p>
+                </div>
+            </div>
        </div>
+       </Link>
     )
 }
