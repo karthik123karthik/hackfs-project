38f1fe5958cd38c71a838066107b13a3ac1ea9c5
sharathguvvala <guvvalasharath@gmail.com>
2022-07-24 17:21:21 +0530

    xmtp

--- my-app/components/chatForm.js
+++ my-app/components/chatForm.js
@@ -1,14 +1,46 @@
 import styles from "../styles/chat.module.css";
+import { Client } from '@xmtp/xmtp-js'
+import { useSigner } from "wagmi";
+import { useState } from "react";
 
 export function ChatForm() {
+
+  const { data: signer } = useSigner();
+  const xmtp = await Client.create(signer);
+
+  const [message, setMessage] = useState("")
+
+  const sendMessage = async () => {
+    try {
+      if (receiverAddress) {
+        console.log(receiverAddress);
+        if (ethers.utils.isAddress(receiverAddress)) {
+          const conversation = await xmtp.conversations.newConversation(
+            receiverAddress
+          );
+          console.log(conversation);
+          if (message) {
+            await conversation.send(message);
+          }
+        } else {
+          console.log("not a valid address");
+        }
+      } else {
+        console.log("receiver address is required");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <section className={styles.chatFormContainer}>
       <section className={styles.sentMessages}>
         {/* TODO: Implement so this container shows all the previous messages */}
       </section>
       <form className={styles.sendContainer}>
-        <input className={styles.messageInput} />
-        <button className={styles.messageSend__btn} type="submit">
+        <input className={styles.messageInput} onChange={(e) => {setMessage(e.target.value)}} />
+        <button className={styles.messageSend__btn} type="submit" onClick={sendMessage} >
           Send
         </button>
       </form>
